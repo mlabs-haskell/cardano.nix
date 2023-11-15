69ed4ece654f27b491f47bf3268b3f80892fa918
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-11-15 13:51:13 +0100

    refactor: `runTestScript` is an option and it's defined in a proper file

--- checks/testing.nix
+++ checks/testing.nix
@@ -11,6 +11,7 @@ in {
       type = types.lazyAttrsOf (types.submodule ({config, ...}: {
         options = {
           name = mkOption {
+            type = types.str;
             default = config._module.args.name;
             internal = true;
           };
@@ -30,112 +31,60 @@ in {
     system,
     pkgs,
     ...
-  }: {
-    ########################################
-    ## Implementation
-    ########################################
-    checks = let
-      # import the testing framework
+  } @ perSystemArgs: {
+    options.cardanoNix.runTestScript = mkOption {
+      type = types.package;
+      default = pkgs.callPackage ./run-test.nix {inherit (config.cardanoNix) tests;};
+      description = "A convenience script to run tests";
+      internal = true;
+    };
+
+    config = let
       nixos-lib = import (inputs.nixpkgs.outPath + "/nixos/lib") {};
-
-      # examine the `systems` attribute of each test, filtering out any that do not support the current system
-      tests = with lib;
-        filterAttrs
-        (_: v: elem system v.systems)
+      tests =
+        lib.filterAttrs
+        (_: v: lib.elem system v.systems)
         config.cardanoNix.tests;
-    in
-      mapAttrs'
-      (name: test:
-        nameValuePair "testing-${test.name}"
-        (nixos-lib.runTest {
-          hostPkgs = pkgs;
 
-          # speed up evaluation by skipping docs
-          defaults.documentation.enable = lib.mkDefault false;
+      inherit (perSystemArgs.config.cardanoNix) runTestScript;
+    in {
+      checks =
+        mapAttrs'
+        (name: test:
+          nameValuePair "testing-${test.name}"
+          (nixos-lib.runTest {
+            hostPkgs = pkgs;
 
-          # make self available in test modules and our custom pkgs
-          node.specialArgs = {
-            inherit pkgs;
-          };
+            # speed up evaluation by skipping docs
+            defaults.documentation.enable = lib.mkDefault false;
 
-          # import all of our flake nixos modules by default
-          defaults.imports = [
-            config.flake.nixosModules.default
-          ];
+            # make self available in test modules and our custom pkgs
+            node.specialArgs = {
+              inherit pkgs;
+            };
 
-          # import the test module
-          imports = [test.module];
-        })
-        .config
-        .result)
-      tests;
+            # import all of our flake nixos modules by default
+            defaults.imports = [
+              config.flake.nixosModules.default
+            ];
 
-    ########################################
-    ## Commands
-    ########################################
-    devshells.default.commands = [
-      {
-        name = "tests";
-        category = "testing";
-        help = "build and run a test";
-        command = with lib; ''
-          help() {
-               # display help
-               echo "  build and run a test"
-               echo
-               echo "  usage:"
-               echo "    test <name>"
-               echo "    test <name> --interactive"
-               echo "    test -s <system> <name>"
-               echo
-               echo "  arguments:"
-               echo "    <name> if a test package is called 'testing-nethermind-basic' then <name> should be 'nethermind-basic'."
-               echo
-               echo "  options:"
-               echo "    -h --help          show this screen."
-               echo "    -l --list          show available tests."
-               echo "    -s --system        specify the target platform [default: x84_64-linux]."
-               echo "    -i --interactive   run the test interactively."
-               echo
-          }
+            # import the test module
+            imports = [test.module];
+          })
+          .config
+          .result)
+        tests;
 
-          list() {
-            # display available tests
-            echo "  list of available tests:"
-            echo
-            echo "${concatMapStrings (s: "    - " + s + "\n") (mapAttrsToList (_: test: test.name) config.cardanoNix.tests)}"
-          }
+      apps.run-test.program = lib.getExe runTestScript;
 
-          args=$(getopt -o lihs: --long list,interactive,help,system: -n 'tests' -- "$@")
-          eval set -- "$args"
-
-          system="${system}"
-          driver_args=()
-
-          while [ $# -gt 0 ]; do
-            case "$1" in
-                -i | --interactive) driver_args+=("--interactive"); shift;;
-                -s | --system) system="$2"; shift 2;;
-                -h | --help) help; exit 0;;
-                -l | --list) list; exit 0;;
-                -- ) shift; break;;
-                * ) break;;
-            esac
-          done
-
-          if [ $# -eq 0 ]; then
-            # no test name has been provided
-            help
-            exit 1
-          fi
-
-          name="$1"
-          shift
-
-          # build/run the test driver, passing any remaining arguments
-          nix run ".#checks.$system.testing-$name.driver" -- "''${driver_args[@]}"
-        '';
-      }
-    ];
+      devshells.default.commands = [
+        {
+          name = "run-test";
+          category = "testing";
+          help = "Run tests";
+          command = "${lib.getExe runTestScript} $@";
+        }
+      ];
+    };
   };
 }
