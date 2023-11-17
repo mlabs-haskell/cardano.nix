1731a95fbc827f5815c70278872522ba21831650
Alexander V. Nikolaev <avn@avnik.info>
2023-11-17 19:15:51 +0200

    Fix hot-reloading server for docs development

--- lib/default.nix
+++ lib/default.nix
@@ -1,5 +1,10 @@
-{lib, ...}: let
+{config, lib, ...}: let
   lib' = import ./functions.nix lib;
 in {
-  config.flake.lib = lib';
+  config = {
+    flake.lib = lib';
+    perSystem = {
+      _module.args.rootConfig = config;
+    };
+  };
 }
