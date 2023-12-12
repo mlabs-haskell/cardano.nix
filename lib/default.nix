f1bec25c654a49d72aa9c1a95f57c4375bfef1b5
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-12-12 14:59:21 +0100

    feat: pass flake's `lib` as arg to flake-parts modules

--- lib/default.nix
+++ lib/default.nix
@@ -5,8 +5,12 @@
 }: {
   config = {
     flake.lib = import ./functions.nix lib;
+    _module.args.lib' = config.lib;
     perSystem = {
-      _module.args.rootConfig = config;
+      _module.args = {
+        rootConfig = config;
+        lib' = config.lib;
+      };
     };
   };
 }
