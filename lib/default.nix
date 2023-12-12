774fa4f846c51d8a138d19502f554b647f73af92
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-12-12 14:57:42 +0100

    refactor: remove unused `let`

--- lib/default.nix
+++ lib/default.nix
@@ -2,11 +2,9 @@
   config,
   lib,
   ...
-}: let
-  lib' = import ./functions.nix lib;
-in {
+}: {
   config = {
-    flake.lib = lib';
+    flake.lib = import ./functions.nix lib;
     perSystem = {
       _module.args.rootConfig = config;
     };
