bbe2ebfd8d58cc2b706e4d7207d71df5704ec1e4
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-12-12 14:54:31 +0100

    refactor: avoid `with`

--- modules/cardano-cli/default.nix
+++ modules/cardano-cli/default.nix
@@ -3,13 +3,12 @@
   lib,
   pkgs,
   ...
-}:
-with lib; {
-  options.cardanoNix.cardano-cli.enable = mkEnableOption "Install cardano CLI tools and scripts";
+}: {
+  options.cardanoNix.cardano-cli.enable = lib.mkEnableOption "Install cardano CLI tools and scripts";
 
-  config = mkIf config.cardanoNix.cardano-cli.enable {
-    environment.systemPackages = with pkgs; [
-      git # FIXME: just a placeholder
+  config = lib.mkIf config.cardanoNix.cardano-cli.enable {
+    environment.systemPackages = [
+      pkgs.git # FIXME: just a placeholder
     ];
   };
 }
