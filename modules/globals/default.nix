d262cd781d166a7618ccfeaa3d35254534f714aa
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-12-18 19:27:18 +0100

    fix: correctly escape `$`

--- modules/globals/default.nix
+++ modules/globals/default.nix
@@ -20,7 +20,7 @@ in
       networkNumber = lib.mkOption {
         type = types.int;
         default = cfg.networkNumbers.${cfg.network};
-        defaultText = lib.literalExpression "config.cardanoNix.globals.networkNumbers.$${config.cardanoNix.globals.net}";
+        defaultText = lib.literalExpression "config.cardanoNix.globals.networkNumbers.\${config.cardanoNix.globals.net}";
         description = ''
           Cardano network number to join/use (should match cardanoNix.globals,network)
         '';
