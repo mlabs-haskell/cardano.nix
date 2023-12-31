235f262d1bfbcb07dec0bb6696de727a5b7338ea
Alexander V. Nikolaev <avn@avnik.info>
2023-12-31 16:14:19 +0200

    feat: Add pre-commit-hooks.nix

--- shell/default.nix
+++ shell/default.nix
@@ -1,6 +1,11 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   imports = [
     inputs.devshell.flakeModule
+    inputs.pre-commit-hooks-nix.flakeModule
   ];
 
   perSystem = {
@@ -15,12 +20,19 @@
           ❄️ Welcome to the {14}{bold}cardano.nix{reset} devshell ❄️
           $(type -p menu &>/dev/null && menu)
         '';
+        startup = {
+          pre-commit = lib.noDepEntry "eval ${config.pre-commit.installationScript}";
+        };
       };
       packages = with pkgs; [
         statix
         config.treefmt.build.wrapper
         reuse
       ];
+    };
+    pre-commit.settings = {
+      hooks.treefmt.enable = true;
+      settings.treefmt.package = config.treefmt.build.wrapper;
     };
   };
 }
