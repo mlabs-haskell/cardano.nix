c51c0e0250a6d8d7d8560a083b51ccef4d965ee8
Alexander V. Nikolaev <avn@avnik.info>
2024-02-04 21:34:41 +0200

    Add real cardano

--- flake.nix
+++ flake.nix
@@ -6,6 +6,15 @@
     # we use effects for CI, documentation and pushing to public cache
     hercules-ci-effects.url = "github:mlabs-haskell/hercules-ci-effects/push-cache-effect";
 
+    # Cardano
+    cardano-node.url = "github:intersectmbo/cardano-node?ref=8.7.3";
+    iohk-nix.follows = "cardano-node/iohkNix";
+
+    # Reduce stackage.nix source download deps
+    haskell-nix.follows = "cardano-node/haskellNix";
+    haskell-nix.inputs.stackage.follows = "empty-flake";
+    empty-flake.url = "github:input-output-hk/empty-flake";
+
     # Utilities
     devshell = {
       url = "github:numtide/devshell";
@@ -42,6 +51,7 @@
         ./docs
         ./formatter
         ./modules
+        ./packages
         ./shell
         ./tests
       ];
