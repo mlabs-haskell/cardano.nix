a227519004b681f18ee0e686a9072981ae2f7965
Alexander V. Nikolaev <avn@avnik.info>
2024-02-05 21:35:45 +0200

    Use real cardano-cli

--- modules/cardano-cli/default.nix
+++ modules/cardano-cli/default.nix
@@ -1,14 +1,18 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  options.cardanoNix.cardano-cli.enable = lib.mkEnableOption "Install cardano CLI tools and scripts";
+{self, ...}: {
+  flake.nixosModules.cardano-cli = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    inherit (pkgs.stdenv.hostPlatform) system;
+  in {
+    options.cardanoNix.cardano-cli.enable = lib.mkEnableOption "Install cardano CLI tools and scripts";
 
-  config = lib.mkIf config.cardanoNix.cardano-cli.enable {
-    environment.systemPackages = [
-      pkgs.git # TODO: replace with `cardano-cli` (milestone 2)
-    ];
+    config = lib.mkIf config.cardanoNix.cardano-cli.enable {
+      environment.systemPackages = [
+        self.packages.${system}.cardano-cli
+      ];
+    };
   };
 }
--- modules/default.nix
+++ modules/default.nix
@@ -1,7 +1,7 @@
 {config, ...}: {
+  imports = [./cardano-cli];
   flake.nixosModules = {
     globals = ./globals;
-    cardano-cli = ./cardano-cli;
     # the default module imports all modules
     default = {
       imports = with builtins; attrValues (removeAttrs config.flake.nixosModules ["default"]);
