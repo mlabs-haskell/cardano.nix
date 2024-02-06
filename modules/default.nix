dc2d2fb7d99bfc56d4d8d34479e077de5c9b1713
Alexander V. Nikolaev <avn@avnik.info>
2024-02-06 16:50:45 +0200

    Add packages nixos module with default package

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,15 @@
 {config, ...}: {
-  imports = [./cardano-cli];
+  # FIXME: two approaches
+  #        1st -- we keep all modules in `parts` level, to have access to self/packages/etc
+  #          cardano-cli is example of first approach
+  #        2nd -- we keep only ./packages.nix in `parts` level, because it require access to self.packages/self.inputs
+  #          package.nix re-export packages as `cardanoNix.packages` options set.
+  imports = [
+    ./cardano-cli
+    ./packages.nix
+  ];
   flake.nixosModules = {
+    # FIXME for second approach all modules except packages.nix should be imported here
     globals = ./globals;
     # the default module imports all modules
     default = {
