501442ee21002bb858d5f15b111dc7743deea27f
Alexander V. Nikolaev <avn@avnik.info>
2024-02-10 18:19:30 +0200

    WIP: refactor module loading

--- modules/cardano-cli/default.nix
+++ modules/cardano-cli/default.nix
@@ -1,18 +1,14 @@
-{self, ...}: {
-  flake.nixosModules.cardano-cli = {
-    config,
-    lib,
-    pkgs,
-    ...
-  }: let
-    inherit (pkgs.stdenv.hostPlatform) system;
-  in {
-    options.cardanoNix.cardano-cli.enable = lib.mkEnableOption "Install cardano CLI tools and scripts";
+{
+  config,
+  self',
+  lib,
+  ...
+}: {
+  options.cardanoNix.cardano-cli.enable = lib.mkEnableOption "Install cardano CLI tools and scripts";
 
-    config = lib.mkIf config.cardanoNix.cardano-cli.enable {
-      environment.systemPackages = [
-        self.packages.${system}.cardano-cli
-      ];
-    };
+  config = lib.mkIf config.cardanoNix.cardano-cli.enable {
+    environment.systemPackages = [
+      self'.packages.cardano-cli
+    ];
   };
 }
