b285b2404c095911c7136dd2f5f3ae4ba75fa95e
Márton Boros <martonboros@gmail.com>
2024-03-27 17:59:11 +0000

    fix typo

--- packages/cardano.nix
+++ packages/cardano.nix
@@ -7,7 +7,7 @@
     packages = lib.filterAttrs (_: v: v != null) {
       cardano-cli = inputs'.cardano-node.packages.cardano-cli or null;
       cardano-node = inputs'.cardano-node.packages.cardano-node or null;
-      inherit (inputs') cardano-configurations;
+      inherit (inputs) cardano-configurations;
     };
   };
 }
