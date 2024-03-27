6e789cddfa4a8255c84cc425524d43449b6c707d
Márton Boros <martonboros@gmail.com>
2024-03-27 19:25:12 +0000

    fix evaluation error

--- packages/cardano.nix
+++ packages/cardano.nix
@@ -3,10 +3,10 @@
   lib,
   ...
 }: {
-  perSystem = {inputs', ...}: {
+  perSystem = {system, ...}: {
     packages = lib.filterAttrs (_: v: v != null) {
-      cardano-cli = inputs'.cardano-node.packages.cardano-cli or null;
-      cardano-node = inputs'.cardano-node.packages.cardano-node or null;
+      cardano-cli = inputs.cardano-node.packages.${system}.cardano-cli or null;
+      cardano-node = inputs.cardano-node.packages.${system}.cardano-node or null;
       inherit (inputs) cardano-configurations;
     };
   };
