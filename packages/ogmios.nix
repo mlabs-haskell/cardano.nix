60c0368e4a0a4928f8083faa0060c84a9fcf62c4
Márton Boros <martonboros@gmail.com>
2024-03-28 19:29:02 +0000

    update to ogmios 6.2.0

--- packages/ogmios.nix
+++ packages/ogmios.nix
@@ -7,16 +7,14 @@
     # Doesn't buid with haskell.nix but master contains a fix. Move to haskell.nix build on next release.
     packages.ogmios = pkgs.stdenv.mkDerivation rec {
       pname = "ogmios";
-      version = "6.1.0";
+      version = "6.2.0";
       src = pkgs.fetchurl {
         url = "https://github.com/CardanoSolutions/ogmios/releases/download/v${version}/ogmios-v${version}-${system}.zip";
         hash =
           if system == "x86_64-linux"
-          then "sha256-JQJTaws+gihwHpTBtUqcNhgbhFDUAdiiXrO2kMX4ZkY="
+          then "sha256-Ryfuzu7JzbR7ivh2Sl9xyOuWh4btjCVSPhXfDLmepHk=u"
           else if system == "aarch64-linux"
-          then "sha256-gWNTzUZHdp5rvbU0aIA3/GJ+YZeFx66h05ugJN4kMco="
-          else if system == "aarch64-darwin"
-          then "sha256-gWNTzUZHdp5rvbU0aIA3/GJ+YZeFx66h05ugJN4kMco="
+          then "sha256-SJQWbIkXF2e4jJtq2hQFqSPO/EhbmLYeZx3aEaXv/gI="
           else abort "Ogmios release not available for system ${system}";
       };
       nativeBuildInputs = [pkgs.unzip];
