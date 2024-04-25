fd311afcc76c5dba691f9ee4283d970bb6e01aa1
zmrocze <karolochmanmilarski@gmail.com>
2024-04-25 17:29:12 +0200

    Fix output check in the test - test passes

--- tests/cardano-db-sync.nix
+++ tests/cardano-db-sync.nix
@@ -17,11 +17,23 @@
         dbname = cfg.cardano.cardano-db-sync.postgres.database;
         inherit (cfg.services.cardano-db-sync.postgres) socketdir;
         # get sync percentage, return true if it's above 0.000001
-        sql = "select (100 * (extract (epoch from (max (time) at time zone 'UTC')) - extract (epoch from (min (time) at time zone 'UTC'))) / (extract (epoch from (now () at time zone 'UTC')) - extract (epoch from (min (time) at time zone 'UTC')))) > 0.000001 from block limit 1;";
-        output = "?column?\\n----------\\nt\\n(1 row)"; # postgres "true" result
+        sql = "select (100 * (extract (epoch from (max (time) at time zone 'UTC')) - extract (epoch from (min (time) at time zone 'UTC'))) / (extract (epoch from (now () at time zone 'UTC')) - extract (epoch from (min (time) at time zone 'UTC')))) > 0.00000001 from block limit 1;";
+        output = " ?column? \\n----------\\n t\\n(1 row)\\n\\n"; # postgres "true" result
       in ''
+        import time
         machine.wait_for_unit("cardano-db-sync")
-        machine.wait_until_succeeds(r"""[[ $(sudo -u postgres psql --no-password "host=${socketdir} user=postgres dbname=${dbname}" -c "${sql}") = "${output}" ]] """, 150)
+        i = 0
+        timeout = 100
+        while True:
+          (status, output) = machine.execute(r"""sudo -u postgres psql --no-password "host=${socketdir} user=postgres dbname=${dbname}" -c "${sql}" """)
+          if i >= timeout:
+            print("Can't wait forever for the dbsync to reach 0.000001. Exiting - dbsync doesn't seem to sync.")
+            raise Exception("Timeout")
+          elif status == 0 and output == "${output}":
+            print("DbSync started syncing. Succeeds.")
+            break
+          i += 1
+          time.sleep(1)
       '';
     };
   };
