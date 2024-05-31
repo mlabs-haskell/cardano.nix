4c4e8b4deba0ea24ffc846828c4acaa821546c86
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-05-31 16:05:36 +0200

    Make blockfrost module pass initial test
    Co-authored-by: Andrea Ciceri <andrea.ciceri@autistici.org>
    Co-authored-by: Alexander V. Nikolaev <avn@avnik.info>
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

--- modules/blockfrost.nix
+++ modules/blockfrost.nix
@@ -24,8 +24,47 @@ in {
           server.debug = true;
           dbSync = {
             inherit (dbsync-cfg.postgres) user port database;
+            host = dbsync-cfg.postgres.socketdir; # Required, to force connect by local socket, as well as local auth
           };
         };
+      };
+      services.postgresql = {
+        identMap = ''
+          users ${config.services.blockfrost.user} ${config.services.cardano-db-sync.postgres.user}
+          users postgres postgres
+        '';
+        authentication = ''
+          local all all ident map=users
+        '';
+      };
+      systemd.services.blockfrost-backend-ryo.serviceConfig = {
+        # Security
+        UMask = "0077";
+        CapabilityBoundingSet = "";
+        ProtectClock = true;
+        ProtectKernelLogs = true;
+        ProtectDevices = true;
+        ProtectKernelModules = true;
+        SystemCallArchitectures = "native";
+        # FIXME: Turn MemoryDenyWriteExecute prevent service from work
+        #          MemoryDenyWriteExecute = true;
+        RestrictNamespaces = true;
+        ProtectHostname = true;
+        ProtectKernelTunables = true;
+        RestrictRealtime = true;
+        # FIXME: Turn SystemCallFilter prevent service from work
+        #          SystemCallFilter = ["@system-service" "~@privileged"];
+        PrivateDevices = true;
+        RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6";
+        ProtectHome = true;
+        DevicePolicy = "closed";
+        DeviceAllow = "";
+        ProtectProc = "invisible";
+        ProcSubset = "pid";
+        PrivateTmp = true;
+        ProtectControlGroups = true;
+        PrivateUsers = true;
+        LockPersonality = true;
       };
     }
     (mkIf (config.cardano.node.enable or false) {
