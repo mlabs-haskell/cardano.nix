d4afd81789a1f28338e03ea4385491cb0e7637aa
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-05-31 16:05:36 +0200

    Make blockfrost module pass initial test
    Co-authored-by: Andrea Ciceri <andrea.ciceri@autistici.org>
    Co-authored-by: Alexander V. Nikolaev <avn@avnik.info>
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

--- tests/blockfrost.nix
+++ tests/blockfrost.nix
@@ -8,6 +8,7 @@
           cli.enable = true;
           node.enable = true;
           db-sync.enable = true;
+          db-sync.postgres.enable = true;
           blockfrost.enable = true;
         };
 
@@ -17,15 +18,12 @@
       testScript = {nodes, ...}: let
         magic = toString nodes.machine.config.cardano.networkNumber;
       in ''
-        import time
         machine.wait_for_unit("cardano-node")
         machine.wait_for_unit("cardano-node-socket")
         machine.wait_until_succeeds("""[[ $(echo "$(cardano-cli query tip --testnet-magic ${magic} | jq '.syncProgress' --raw-output) > 0.001" | bc) == "1" ]]""")
         machine.wait_for_unit("blockfrost-backend-ryo")
-        time.sleep(10)
-        machine.succeed("curl http://localhost:3000/health")
-        machine.succeed("curl --silent --fail http://localhost:3000/health")
-        print(machine.succeed("systemd-analyze security blockfrost"))
+        machine.wait_until_succeeds("curl --fail http://localhost:3000/health", timeout=60)
+        print(machine.succeed("systemd-analyze security blockfrost-backend-ryo"))
       '';
     };
   };
