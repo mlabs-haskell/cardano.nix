fd189e75804c20099ec49103efb5fd0f329bcfa5
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-07-05 17:50:03 +0200

    Remove unneded hack
    It was causing issues with package options having
    as default values packages not in `pkgs`

--- docs/render.nix
+++ docs/render.nix
@@ -85,6 +85,7 @@ in {
       config,
       lib,
       pkgs,
+      system,
       ...
     }: let
       inherit (pkgs) stdenv mkdocs python311Packages;
@@ -150,8 +151,14 @@ in {
                 );
               options = let
                 evaluated = lib.evalModules {
-                  modules = (import "${inputs.nixpkgs}/nixos/modules/module-list.nix") ++ modules;
-                  specialArgs = {inherit pkgs;};
+                  modules =
+                    modules
+                    ++ [
+                      {
+                        imports = builtins.import "${inputs.nixpkgs}/nixos/modules/module-list.nix";
+                        nixpkgs.system = system;
+                      }
+                    ];
                 };
               in
                 lib.foldr (path: acc: lib.recursiveUpdate acc (lib.attrByPath (lib.splitString "." path) {} evaluated.options)) {} namespaces;
