69333d47830c930669fefc273454030dd3f45f84
Alexander V. Nikolaev <avn@avnik.info>
2024-07-19 19:15:35 +0300

    Enable oura plugins
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

--- packages/oura.nix
+++ packages/oura.nix
@@ -2,6 +2,8 @@
   perSystem = {pkgs, ...}: let
     craneLib = inputs.crane.mkLib pkgs;
     oura = craneLib.buildPackage {
+      cargoExtraArgs = "--all-features"; # Enable all bundled plugins
+      nativeBuildInputs = [pkgs.perl]; # Adding perl fixing build of vendored openssl inside `openssl-sys` crate
       src = craneLib.cleanCargoSource inputs.oura;
     };
   in {
