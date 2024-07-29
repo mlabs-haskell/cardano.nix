5f99673afe5cdb02cf7d93fda129f5fbd4c74594
Alexander V. Nikolaev <avn@avnik.info>
2024-07-29 19:22:02 +0300

    oura: Build with system openssl library
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

--- packages/oura.nix
+++ packages/oura.nix
@@ -3,7 +3,11 @@
     craneLib = inputs.crane.mkLib pkgs;
     oura = craneLib.buildPackage {
       cargoExtraArgs = "--all-features"; # Enable all bundled plugins
-      nativeBuildInputs = [pkgs.perl]; # Adding perl fixing build of vendored openssl inside `openssl-sys` crate
+      env = {
+        OPENSSL_NO_VENDOR = "1"; # Use system openssl
+      };
+      nativeBuildInputs = [pkgs.pkg-config];
+      buildInputs = [pkgs.openssl];
       src = craneLib.cleanCargoSource inputs.oura;
     };
   in {
