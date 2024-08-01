876d3a89a817bcaaedad2a43deedf79b94f2c294
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-08-01 16:14:47 +0200

    Restore `cardano-configurations`

--- packages/default.nix
+++ packages/default.nix
@@ -32,6 +32,7 @@
     in {
       cardano-cli = packages."cardano-cli-8.1.1";
       cardano-node = packages."cardano-node-8.1.1";
+      cardano-configurations = packages."cardano-configurations-8.1.1";
       ogmios = packages."ogmios-6.0.3";
       kupo = packages."kupo-2.6.1";
       oura = packages."oura-1.8.6";
