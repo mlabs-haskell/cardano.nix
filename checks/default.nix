4fba0c9ddb3476fac672a1b4cf6a50fb04aaafa9
Márton Boros <martonboros@gmail.com>
2024-11-18 16:13:05 +0000

    use nixfmt

--- checks/default.nix
+++ checks/default.nix
@@ -1,42 +1,48 @@
-{inputs, ...}: {
+{
+  inputs,
+  ...
+}:
+{
   imports = [
     ./vmTests.nix
     ./licenses.nix
   ];
-  perSystem = {
-    pkgs,
-    config,
-    ...
-  }: {
-    apps = {
-      nix-build-all.program = pkgs.writeShellApplication {
-        name = "nix-build-all";
-        runtimeInputs = [
-          (pkgs.callPackage inputs.devour-flake {})
-        ];
-        text = ''
-          # Make sure that flake.lock is sync
-          nix flake lock --no-update-lock-file
+  perSystem =
+    {
+      pkgs,
+      config,
+      ...
+    }:
+    {
+      apps = {
+        nix-build-all.program = pkgs.writeShellApplication {
+          name = "nix-build-all";
+          runtimeInputs = [
+            (pkgs.callPackage inputs.devour-flake { })
+          ];
+          text = ''
+            # Make sure that flake.lock is sync
+            nix flake lock --no-update-lock-file
 
-          # Do a full nix build (all outputs)
-          devour-flake . "$@"
-        '';
+            # Do a full nix build (all outputs)
+            devour-flake . "$@"
+          '';
+        };
       };
-    };
 
-    devshells.default.commands = [
-      {
-        category = "tests";
-        name = "build-all";
-        help = "build all packages and checks with `devour-flake`";
-        command = config.apps.nix-build-all.program;
-      }
-      {
-        category = "tests";
-        name = "check";
-        help = "run `nix flake check`";
-        command = "nix flake check";
-      }
-    ];
-  };
+      devshells.default.commands = [
+        {
+          category = "tests";
+          name = "build-all";
+          help = "build all packages and checks with `devour-flake`";
+          command = config.apps.nix-build-all.program;
+        }
+        {
+          category = "tests";
+          name = "check";
+          help = "run `nix flake check`";
+          command = "nix flake check";
+        }
+      ];
+    };
 }
--- formatter/default.nix
+++ formatter/default.nix
@@ -1,35 +1,39 @@
-{inputs, ...}: {
+{ inputs, ... }:
+{
   imports = [
     inputs.flake-root.flakeModule
     inputs.treefmt-nix.flakeModule
   ];
 
-  perSystem = {
-    config,
-    pkgs,
-    lib,
-    ...
-  }: {
-    treefmt.config = {
-      inherit (config.flake-root) projectRootFile;
-      package = pkgs.treefmt;
-      flakeFormatter = true;
-      flakeCheck = true;
-      programs = {
-        alejandra.enable = true;
-        deadnix.enable = true;
-        prettier.enable = true;
-        statix.enable = true;
+  perSystem =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      treefmt.config = {
+        inherit (config.flake-root) projectRootFile;
+        package = pkgs.treefmt;
+        flakeFormatter = true;
+        flakeCheck = true;
+        programs = {
+          nixfmt.enable = true;
+          deadnix.enable = true;
+          prettier.enable = true;
+          statix.enable = true;
+        };
+        settings.formatter.nixfmt.options = [ "--width=65536" ];
       };
-    };
 
-    devshells.default.commands = [
-      {
-        category = "tools";
-        name = "fmt";
-        help = "format the source tree";
-        command = lib.getExe config.treefmt.build.wrapper;
-      }
-    ];
-  };
+      devshells.default.commands = [
+        {
+          category = "tools";
+          name = "fmt";
+          help = "format the source tree";
+          command = lib.getExe config.treefmt.build.wrapper;
+        }
+      ];
+    };
 }
