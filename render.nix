d5184ad1723f61342f85fcc955e5f12ae37a94b3
Hercules CI Effects <hercules-ci[bot]@users.noreply.github.com>
2024-11-19 15:10:05 +0000

    Update gh-pages
    Store path: /nix/store/y1aqb1f1cd4gzzydfzh4xibc5p741s9v-cardano-nix-docs

--- render.nix
+++ render.nix
@@ -4,11 +4,24 @@
   lib,
   self,
   ...
-}: let
+}:
+let
   cfg = config.renderDocs;
 
-  inherit (lib) mkOption mkEnableOption mkIf mkMerge;
-  inherit (lib.types) bool str listOf deferredModule submodule path;
+  inherit (lib)
+    mkOption
+    mkEnableOption
+    mkIf
+    mkMerge
+    ;
+  inherit (lib.types)
+    bool
+    str
+    listOf
+    deferredModule
+    submodule
+    path
+    ;
 
   sidebarType = submodule {
     options = {
@@ -51,7 +64,8 @@
       };
     };
   };
-in {
+in
+{
   options.renderDocs = {
     enable = mkEnableOption "Document rendering";
 
@@ -88,7 +102,7 @@ in {
 
     devshells = mkOption {
       type = listOf str;
-      default = ["default"];
+      default = [ "default" ];
       description = ''
         Names of the devshells to add `docs-serve` and `docs-build` commands to
       '';
@@ -96,7 +110,7 @@ in {
 
     sidebarOptions = mkOption {
       type = listOf sidebarType;
-      default = [];
+      default = [ ];
       description = ''
         List of reference sections
       '';
@@ -104,7 +118,7 @@ in {
 
     fixups = mkOption {
       type = listOf fixupType;
-      default = [];
+      default = [ ];
     };
 
     invisible = mkOption {
@@ -117,138 +131,147 @@ in {
   };
 
   config = mkIf cfg.enable {
-    perSystem = {
-      config,
-      lib,
-      pkgs,
-      system,
-      ...
-    }: let
-      inherit (pkgs) stdenv mkdocs python312Packages;
+    perSystem =
+      {
+        config,
+        lib,
+        pkgs,
+        system,
+        ...
+      }:
+      let
+        inherit (pkgs) stdenv mkdocs python312Packages;
 
-      my-mkdocs =
-        pkgs.runCommand "my-mkdocs"
-        {
-          buildInputs = [
-            mkdocs
-            python312Packages.mkdocs-material
-          ];
-        } ''
-          mkdir -p $out/bin
+        my-mkdocs =
+          pkgs.runCommand "my-mkdocs"
+            {
+              buildInputs = [
+                mkdocs
+                python312Packages.mkdocs-material
+              ];
+            }
+            ''
+              mkdir -p $out/bin
 
-          cat <<MKDOCS > $out/bin/mkdocs
-          #!${pkgs.bash}/bin/bash
-          set -euo pipefail
-          export PYTHONPATH=$PYTHONPATH
-          exec ${mkdocs}/bin/mkdocs "\$@"
-          MKDOCS
+              cat <<MKDOCS > $out/bin/mkdocs
+              #!${pkgs.bash}/bin/bash
+              set -euo pipefail
+              export PYTHONPATH=$PYTHONPATH
+              exec ${mkdocs}/bin/mkdocs "\$@"
+              MKDOCS
 
-          chmod +x $out/bin/mkdocs
-        '';
+              chmod +x $out/bin/mkdocs
+            '';
 
-      eachOptionsDoc = builtins.listToAttrs (builtins.map (
-          {
-            anchor,
-            modules,
-            namespaces,
-            ...
-          }:
-            lib.nameValuePair
-            anchor
-            (pkgs.nixosOptionsDoc {
-              # By default `nixosOptionsDoc` will ignore internal options but we want to show them
-              # This hack will make all the options not internal and visible and optionally append to the
-              # description a new field which is then corrected rendered as it was a native field
-              transformOptions = opt:
-                opt
-                // lib.optionalAttrs cfg.invisible {
-                  internal = false;
-                  visible = true;
-                }
-                // {
-                  description = ''
-                    ${lib.optionalString (opt.description != null) opt.description}
-                    ${lib.optionalString (opt.internal && cfg.invisible) "*Internal:* true"}
-                  '';
-                }
-                // (
-                  if (lib.length (lib.splitString "\n" opt.default.text or "") > 20)
-                  then {
-                    default._type = "literalMD";
-                    default.text = ''
-                      <details><summary>This value is long. Click to expand.</summary>
-                      ```nix
-                      ${opt.default.text}
-                      ```
-                      </details>
+        eachOptionsDoc = builtins.listToAttrs (
+          builtins.map (
+            {
+              anchor,
+              modules,
+              namespaces,
+              ...
+            }:
+            lib.nameValuePair anchor (
+              pkgs.nixosOptionsDoc {
+                # By default `nixosOptionsDoc` will ignore internal options but we want to show them
+                # This hack will make all the options not internal and visible and optionally append to the
+                # description a new field which is then corrected rendered as it was a native field
+                transformOptions =
+                  opt:
+                  opt
+                  // lib.optionalAttrs cfg.invisible {
+                    internal = false;
+                    visible = true;
+                  }
+                  // {
+                    description = ''
+                      ${lib.optionalString (opt.description != null) opt.description}
+                      ${lib.optionalString (opt.internal && cfg.invisible) "*Internal:* true"}
                     '';
                   }
-                  else {}
-                );
-              options = let
-                evaluated = lib.evalModules {
-                  modules =
-                    modules
-                    ++ [
+                  // (
+                    if (lib.length (lib.splitString "\n" opt.default.text or "") > 20) then
                       {
-                        imports = builtins.import "${inputs.nixpkgs}/nixos/modules/module-list.nix";
-                        nixpkgs.system = system;
+                        default._type = "literalMD";
+                        default.text = ''
+                          <details><summary>This value is long. Click to expand.</summary>
+                          ```nix
+                          ${opt.default.text}
+                          ```
+                          </details>
+                        '';
                       }
-                    ];
-                };
-              in
-                lib.foldr (path: acc: lib.recursiveUpdate acc (lib.attrByPath (lib.splitString "." path) {} evaluated.options)) {} namespaces;
-            })
-        )
-        cfg.sidebarOptions);
+                    else
+                      { }
+                  );
+                options =
+                  let
+                    evaluated = lib.evalModules {
+                      modules = modules ++ [
+                        {
+                          imports = builtins.import "${inputs.nixpkgs}/nixos/modules/module-list.nix";
+                          nixpkgs.system = system;
+                        }
+                      ];
+                    };
+                  in
+                  lib.foldr (path: acc: lib.recursiveUpdate acc (lib.attrByPath (lib.splitString "." path) { } evaluated.options)) { } namespaces;
+              }
+            )
+          ) cfg.sidebarOptions
+        );
 
-      statements =
-        lib.concatStringsSep "\n"
-        (lib.mapAttrsToList (n: v: ''
+        statements = lib.concatStringsSep "\n" (
+          lib.mapAttrsToList (n: v: ''
             path=$out/${n}.md
             cat ${v.optionsCommonMark} | sed 's/\\</</g' | sed 's/This value is long\\. Click to expand\\./This value is long. Click to expand./g' >> $path
-          '')
-          eachOptionsDoc);
+          '') eachOptionsDoc
+        );
 
-      options-doc = pkgs.runCommand "nixos-options" {} ''
-        mkdir $out
-        ${statements}
-        # Replace `/nix/store` related paths with public urls
-        find $out -type f | xargs -n1 sed -i ${lib.concatMapStrings (x: " -e 's,${x.storePath},${x.githubUrl},g'") cfg.fixups} -e "s,file://https://,https://,g"
-      '';
-
-      docsPath = "./docs/reference/module-options";
-
-      index = {
-        nav = [
-          {
-            "NixOS Module Reference" = lib.mapAttrsToList (n: _: {${n} = "reference/module-options/${n}.md";}) eachOptionsDoc;
-          }
-        ];
-      };
-
-      indexYAML =
-        pkgs.runCommand "index.yaml" {
-          nativeBuildInputs = [pkgs.yq-go];
-          index = builtins.toFile "index.json" (builtins.unsafeDiscardStringContext (builtins.toJSON index));
-        } ''
-          yq -o yaml $index >$out
+        options-doc = pkgs.runCommand "nixos-options" { } ''
+          mkdir $out
+          ${statements}
+          # Replace `/nix/store` related paths with public urls
+          find $out -type f | xargs -n1 sed -i ${lib.concatMapStrings (x: " -e 's,${x.storePath},${x.githubUrl},g'") cfg.fixups} -e "s,file://https://,https://,g"
         '';
 
-      mergedMkdocsYaml =
-        pkgs.runCommand "mkdocs.yaml" {
-          nativeBuildInputs = [pkgs.yq-go];
-        } ''
-          yq '. *+ load("${indexYAML}")' ${cfg.mkdocsYamlFile} -o yaml >$out
-        '';
-    in
-      mkMerge ([
+        docsPath = "./docs/reference/module-options";
+
+        index = {
+          nav = [
+            {
+              "NixOS Module Reference" = lib.mapAttrsToList (n: _: { ${n} = "reference/module-options/${n}.md"; }) eachOptionsDoc;
+            }
+          ];
+        };
+
+        indexYAML =
+          pkgs.runCommand "index.yaml"
+            {
+              nativeBuildInputs = [ pkgs.yq-go ];
+              index = builtins.toFile "index.json" (builtins.unsafeDiscardStringContext (builtins.toJSON index));
+            }
+            ''
+              yq -o yaml $index >$out
+            '';
+
+        mergedMkdocsYaml =
+          pkgs.runCommand "mkdocs.yaml"
+            {
+              nativeBuildInputs = [ pkgs.yq-go ];
+            }
+            ''
+              yq '. *+ load("${indexYAML}")' ${cfg.mkdocsYamlFile} -o yaml >$out
+            '';
+      in
+      mkMerge (
+        [
           {
             packages.${cfg.packageName} = stdenv.mkDerivation {
               src = cfg.directory;
               inherit (cfg) name;
 
-              nativeBuildInputs = [my-mkdocs];
+              nativeBuildInputs = [ my-mkdocs ];
 
               unpackPhase = ''
                 cp -r $src docs
@@ -291,12 +314,13 @@ in {
             packages."${cfg.packageName}-serve" = config.packages.${cfg.packageName}.serve;
           }
         ]
-        ++ (
-          builtins.map (devshell: {
-            devshells."${devshell}" = {
-              commands = let
+        ++ (builtins.map (devshell: {
+          devshells."${devshell}" = {
+            commands =
+              let
                 category = "documentation";
-              in [
+              in
+              [
                 {
                   inherit category;
                   name = "docs-serve";
@@ -310,12 +334,11 @@ in {
                   command = "nix build .#${cfg.packageName}";
                 }
               ];
-              packages = [
-                my-mkdocs
-              ];
-            };
-          })
-          cfg.devshells
-        ));
+            packages = [
+              my-mkdocs
+            ];
+          };
+        }) cfg.devshells)
+      );
   };
 }
