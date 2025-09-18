ff6038b0fef758a2c11710c41238b84e4946b5c2
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-09-18 22:08:30 +0200

    style: remove unnnecessary comment

--- flake.nix
+++ flake.nix
@@ -110,14 +110,6 @@
           description = "Example flake for deploying a cardano.nix cluster with multiple nodes, load balancer and monitoring";
         };
       };
-      systems = [
-        "x86_64-linux"
-        # cardano-node doesn't support it
-        # "aarch64-linux"
-        # ogmios doesn't support it
-        # "x86_64-darwin"
-        # we don't have a builder
-        # "aarch64-darwin"
-      ];
+      systems = [ "x86_64-linux" ];
     };
 }
